997a49dedd507b622eaecb97e0e582e9f1b6ab4e
Pafcal <a.pavaloi@gmail.com>
2018-05-02 08:36:40 +0300

    Added exercises for Week#10

---
+++ week#10/part1/script.js
@@ -0,0 +1,43 @@
+/** ⚡ DEMO: JS crash-course 🚓 */
+
+let total = '123 dollars'; // variable declaration
+total = 22; 
+
+console.log(document.body); // print to the console + document object
+
+
+/** EX #1: using DOM methods add the following elements in the HTML's body 
+ * 
+ * body
+ *  .gallery
+ *      img: src, alt
+ *      img: src, alt
+ *      img: src, alt
+*/
+
+
+/** 💻 Code #1 💻 */
+
+
+/** ⚡ DEMO accessing the DOM & event listeners */
+
+
+/** EX #2: 
+ *   - add a click event listener on every image that prints a message to the console 
+*/
+
+/** 💻 Code  #2 💻  */
+
+/** ⚡ DEMO removing event listeners */
+
+/** EX #3:  add a click event listener on the gallery that prints a message to the console
+ * 
+*/
+
+/** 💻 Code #3 💻 */
+
+
+//  🤚 Q&A: which one triggered first? Bubble vs capture
+
+
+/** ⚡ DEMO:  a more efficient way of handling multiple event listeners */
